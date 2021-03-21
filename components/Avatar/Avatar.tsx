212e3701b09002e86340eb172e3ef6b9c4b0a702
fatihcandev <contact@fatihcan.dev>
2021-03-21 12:44:50 +0300

    create Avatar component and add to Header

---
+++ components/Avatar/Avatar.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import Image from 'next/image'
+
+interface IAvatarProps {
+  size?: 'sm' | 'md'
+  src: string
+  isProfile?: boolean
+  className?: string
+  onClick?: () => void
+}
+
+const Avatar: React.FC<IAvatarProps> = ({ size, src, isProfile, className, onClick }) => {
+  const getSize = () => {
+    if (isProfile) {
+      switch (size) {
+        case 'sm':
+          return 95.5
+        case 'md':
+          return 133.5
+        default:
+          return 67.5
+      }
+    } else {
+      switch (size) {
+        case 'sm':
+          return 40
+        case 'md':
+          return 48
+        default:
+          return 32
+      }
+    }
+  }
+
+  return (
+    <Image src={src} width={getSize()} height={getSize()} className={className} onClick={onClick} />
+  )
+}
+
+export default Avatar
--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { Avatar } from '../Avatar'
 
 import styles from './Header.module.css'
 
-const Header = () => {
-  return <div className={styles.container}>Header</div>
+interface IHeaderProps {
+  className?: string
+  onAvatarClick: () => void
+}
+
+const Header: React.FC<IHeaderProps> = ({ className = '', onAvatarClick }) => {
+  return (
+    <div className={`${styles.container} ${className}`}>
+      <Avatar
+        src="https://pbs.twimg.com/profile_images/1373195730123374593/2098fuZm_400x400.jpg"
+        onClick={onAvatarClick}
+      />
+    </div>
+  )
 }
 
 export default Header
