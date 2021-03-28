f15f015b20be952a8917485c260862e6ab059fcc
Fatih <contact@fatihcan.dev>
2021-03-28 15:03:59 +0300

    add primary variant to Button

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -7,23 +7,25 @@ import { IconName } from '../../types'
 import styles from './Button.module.css'
 
 export interface IButtonProps {
+  type?: 'primary' | 'secondary'
   onClick?: () => void
   href?: string
   leftIcon?: IconName
   rightIcon?: IconName
   iconSize?: string
+  full?: boolean
   className?: string
   children?: React.ReactNode
 }
 
 const Button = React.forwardRef<HTMLAnchorElement, IButtonProps>(
-  ({ onClick, href, leftIcon, rightIcon, iconSize, className = '', children }, ref) => {
+  ({ type, onClick, href, leftIcon, rightIcon, iconSize, full, className = '', children }, ref) => {
     return (
       <a
         role="button"
         href={href}
         onClick={onClick}
-        className={`${styles.container} ${className}`}
+        className={`${styles.container} ${className} ${type && styles[type]} ${full && 'w-full'}`}
         ref={ref}
       >
         {leftIcon && <Icon name={leftIcon} size={iconSize} />}
